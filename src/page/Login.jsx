5b4139f500a9cd10f31437100a3c48a9bc612f2a
lliwIWant <liwil5486@gmail.com>
2025-04-17 22:01:57 +0900

    login

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 const Login = () => {
   return (
     <div>
-        Login
+    <Form className='loginWrap'>
+        <Form.Group className="mb-3" controlId="formGroupEmail">
+            <Form.Label>아이디:</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formGroupPassword">
+            <Form.Label>비밀번호:</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <div className='loginBtnArea'>
+            <Button variant="primary" type="submit" className='loginBtn'>
+            Submit
+        </Button>
+        </div>
+    </Form>
 
     </div>
   )
